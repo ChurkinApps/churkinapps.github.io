6b26ede6efcb68a6250903687a191949360a3f13
Alex Churkin <churkindev@gmail.com>
2021-08-31 21:25:27 +0300

    time in main_page.js changed

--- js/main_page.js
+++ js/main_page.js
@@ -1,5 +1,6 @@
+/* Scroll for "Our apps" button */
 $("#big-button").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#posts").offset().top - 20
-    }, 500);
+    }, 400);
 });
